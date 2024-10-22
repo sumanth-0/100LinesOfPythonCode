6ce89df6da9bc467d6b2f5e2a44cb1b12e6ac52a
Braylon Minnis <150169515+Badger0909@users.noreply.github.com>
2024-10-22 12:58:39 -0700

    number_guesser.py
    final code

--- number_guesser/number_guesser_code.py
+++ number_guesser/number_guesser_code.py
@@ -1,22 +1,29 @@
 import random 
-#assign variables
-randomNum = random.randint(1, 100)
-playing = True
-guesses = 0
-while playing:
-    # start code sequence
-    print(f"Guess {guesses}:")
-    print("What is your guess?")
-    guess = int(input())
+mainGame = True
+while mainGame:
+    #assign variables
+    randomNum = random.randint(1, 100)
+    playing = True
+    guesses = 0
+    while playing:
+        # start code sequence
+        print(f"Guess {guesses}:")
+        print("What is your guess?")
+        guess = int(input())
     
-    # win logic
-    if guess == randomNum:
-      print(f"You won in {guesses} guesses! The number was {randomNum}")
-      playing = False
-    elif guess > randomNum:
-      print("Your guess is over!")
-    else:
-      print("Your guess is under!")
+        # win logic
+        if guess == randomNum:
+            print(f"You won in {guesses} guesses! The number was {randomNum}")
+            playing = False
+        elif guess > randomNum:
+            print("Your guess is over!")
+        else:
+            print("Your guess is under!")
     
-    #adds to guess counter
-    guesses += 1
+        #adds to guess counter
+        guesses += 1
+
+    print("would you like to play again? (Y/N)")
+    answer = input()
+    if answer == "N":
+        mainGame = False
