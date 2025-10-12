1e57a481b7c54f9b7312c860782c73ee79dfbf44
Shamshu123 <63791174+Shamshu123@users.noreply.github.com>
2025-10-12 15:38:12 +0530

    Update dice_battle_simulator.py

--- #782 DICE_BATTLE_SIMULATOR/dice_battle_simulator.py
+++ #782 DICE_BATTLE_SIMULATOR/dice_battle_simulator.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 # Print welcome message.
 print("Welcome to Dice Battle Simulator! \n The game will continue until killed or the goal is reached ;)")
@@ -12,14 +13,20 @@ p1_score, p2_score = 0,0
 scores = [0,0]
 
 for i in range(GOAL):
-    #Roll the die.
-    roll = random.randint(1,6)
-
+    time.sleep(random.randint(10,20)/10)
     # Decide whose turn it is.
     p_id = (i % 2)
 
+    #Roll the die.
+    roll = random.randint(1,6)
+    print(f"Player {p_id+1} rolled {roll}.")
+
     # Terminate the game when the goal is reached. Uses Walrus Operator ;)
-    if (scores[p_id] := scores[p_id] + roll) >= GOAL:
+    scores[p_id] += roll
+    c_score = scores[p_id]
+    if c_score >= GOAL:
         print(f"Player {p_id+1} has won! Congratulations!")
         print(f"The results are: \n Goal: {GOAL} \n Player 1: {scores[0]} \n Player 2: {scores[1]}")
         exit()
+
+    print(f"Player {p_id+1} has a score of {c_score}.")
