947e5af6521afed8cda03bb832952fbe5a2c2b31
Arjun Kumar <arjunk840095@gmail.com>
2025-10-26 20:06:31 +0530

    Add fire effect generator script for  Hacktoberfest
    Added fire Effect Generator for Hacktoberfest

---
+++ #1098_Joke_Of_The_Day/fire_effect_generator.py
@@ -0,0 +1,14 @@
+import random
+import time
+import os
+
+def fire_effect():
+  chars=['.',':',';','!','^']
+  while True:
+    line = ".join(random.choice(chars)for_in range(50))
+    print(line)
+    time.sleep(0.1)
+    os.system('cls'if os.name =='int' else 'clear')
+
+if__name__=="__main__"
+  fire_effect()
